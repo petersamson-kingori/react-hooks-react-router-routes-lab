4c1a39f019d73531d1f2f34ddeb0bacf3f32aeb5
petersamson-kingori <peters.kingori@student.moringaschool.com>
2023-02-17 22:43:11 +0300

    Lab Completed

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,21 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <>
+      <h1>Actors Page</h1>
+      {actors.map((actor, index) => (
+        <div key={index}>
+          <h2>{actor.name}</h2>
+          <ul>
+            {actor.movies.map((movie, index) => (
+              <li key={index}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default Actors;
